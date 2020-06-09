58d6e1288c1879f2f99ac25ffbe3068b083f5cd1
Yash Agrawal <yagrawl2@gmail.com>
2020-06-08 20:56:53 -0700

    remove messaging for toggles

--- extension/client/app/background.js
+++ extension/client/app/background.js
@@ -1,19 +1,3 @@
 console.log("%c Background ",
         "color: white; background-color: #9CADCE",
         `Background.js Initiated`);
-
-chrome.extension.onConnect.addListener(port => {
-  port.onMessage.addListener(message => {
-    console.log("%c Message ",
-            "color: white; background-color: #FF7477",
-            `From Popup to Background: ${message}`);
-    sendMessageToPopup(port, "hello");
-  });
-});
-
-const sendMessageToPopup = (port, message) => {
-  console.log("%c Message ",
-          "color: white; background-color: #FF7477",
-          `From Background to Popup: ${message}`);
-  port.postMessage(message);
-}
--- extension/client/app/popup.js
+++ extension/client/app/popup.js
@@ -1,36 +1,14 @@
-const PORT = chrome.extension.connect({ name: "toggles" });
-
 let recordActivityToggle = document.getElementById("recordActivityToggle");
 let hideViewsToggle = document.getElementById("hideViewsToggle");
 
-localStorage.setItem("recordActivityToggle", "true");
-localStorage.setItem("hideViewsToggle", "false");
-
 recordActivityToggle.addEventListener("change", function() {
-  localStorage.setItem("recordActivityToggle", this.checked);
-  sendMessageToBackground(`recordActivityToggle:${this.checked}`);
   console.log("%c Toggle ",
           "color: white; background-color: #2274A5",
           `recordActivityToggle: updated to ${this.checked}`);
 });
 
 hideViewsToggle.addEventListener("change", function() {
-  localStorage.setItem("hideViewsToggle", this.checked);
-  sendMessageToBackground(`hideViewsToggle:${this.checked}`);
   console.log("%c Toggle ",
           "color: white; background-color: #2274A5",
           `hideViewsToggle: updated to ${this.checked}`);
-});
-
-const sendMessageToBackground = message => {
-  console.log("%c Message ",
-          "color: white; background-color: #FF7477",
-          `From Popup to Background: ${message}`)
-  PORT.postMessage(message);
-}
-
-PORT.onMessage.addListener(message => {
-  console.log("%c Message ",
-          "color: white; background-color: #FF7477",
-          `From Background to Popup: ${message}`)
 });
