bd776581b7a2efff3bf4034e063b79f9fd12710f
Yash Agrawal <yagrawl2@gmail.com>
2020-06-09 23:10:54 -0700

    fix storage.local save in background

--- extension/client/app/background.js
+++ extension/client/app/background.js
@@ -15,18 +15,20 @@ chrome.runtime.onInstalled.addListener(function() {
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if(tab.status === "complete" && changeInfo.status === "complete") {
-    let videoId = tab.url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+    let videoId = tab.url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/)[1];
 
     if(videoId !== null) {
       console.log("%c videoId ", "color: white; background-color: #D7AF70",
                 videoId);
 
       chrome.storage.local.get([ videoId ], result => {
-        if(result[videoId] === undefined) {
+        if(Object.keys(result).length === 0 && result.constructor === Object) {
           chrome.storage.local.set({ [ videoId ]: 1 });
+          console.log("%c Views ", "color: white; background-color: #F56960", 1);
         } else {
           let views = result[videoId] + 1;
           chrome.storage.local.set({ [ videoId ]: views });
+          console.log("%c Views ", "color: white; background-color: #F56960", views);
         }
       });
     }
