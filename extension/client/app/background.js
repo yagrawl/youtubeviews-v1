773d292ea010bc694210b3c2b3064831cba05146
Yash Agrawal <yagrawl2@gmail.com>
2020-06-11 21:35:30 -0700

    log with timestamp

--- extension/client/app/background.js
+++ extension/client/app/background.js
@@ -1,30 +1,26 @@
 let RECORD_ACTIVITY = true;
+let YOUTUBE_VIDEO_ID_LENGTH = 11;
 
 chrome.runtime.onInstalled.addListener(function() {
-  console.log("%c Background ", "color: white; background-color: #9CADCE",
-              `Background.js Initiated`);
+  console.log(`${time()} %c Background `, "color: white; background-color: #9CADCE", `Background.js Initiated`);
 
   chrome.storage.sync.set({ record_activity: true }, () => {
     RECORD_ACTIVITY = true;
-    console.log("%c Toggle ", "color: white; background-color: #2274A5",
-                `Record Activity Toggle set to true`);
+    console.log(`${time()} %c Toggle `, "color: white; background-color: #2274A5", `Record Activity Toggle set to true`);
   });
 
   chrome.storage.sync.set({ hide_views: false }, () => {
-    console.log("%c Toggle ", "color: white; background-color: #2274A5",
-                `Hide Views Toggle set to false`);
+    console.log(`${time()} %c Toggle `, "color: white; background-color: #2274A5", `Hide Views Toggle set to false`);
   });
 });
 
 chrome.storage.onChanged.addListener((changes, area) => {
   if (area == "sync" && "record_activity" in changes) {
     if(changes.record_activity.newValue) {
-      console.log("%c Content ", "color: white; background-color: #FE5F55",
-                  `Record Activity toggle updated to true. Recording Activity.`);
+      console.log(`${time()} %c Content `, "color: white; background-color: #FE5F55", `Record Activity toggle updated to true. Recording Activity.`);
       RECORD_ACTIVITY = true;
     } else {
-      console.log("%c Content ", "color: white; background-color: #FE5F55",
-                  `Record Activity toggle updated to false. Not Recording Activity.`);
+      console.log(`${time()} %c Content `, "color: white; background-color: #FE5F55", `Record Activity toggle updated to false. Not Recording Activity.`);
       RECORD_ACTIVITY = false;
     }
   }
@@ -37,22 +33,38 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
       if(url !== null) {
         let videoId = url[1];
-        console.log("%c videoId ", "color: white; background-color: #D7AF70",
-                  videoId);
+        if(videoId.length === YOUTUBE_VIDEO_ID_LENGTH) {
+          console.log(`${time()} %c videoId `, "color: white; background-color: #D7AF70", `${videoId}`);
 
-        chrome.storage.local.get([ videoId ], result => {
-          if(Object.keys(result).length === 0 && result.constructor === Object) {
-            chrome.storage.local.set({ [ videoId ]: 1 });
-            chrome.tabs.sendMessage(tab.id, { view: { id: videoId, count: 1 }});
-            console.log("%c Views ", "color: white; background-color: #F56960", 1);
-          } else {
-            let views = result[videoId] + 1;
-            chrome.storage.local.set({ [ videoId ]: views });
-            chrome.tabs.sendMessage(tab.id, { view: { id: videoId, count: views }});
-            console.log("%c Views ", "color: white; background-color: #F56960", views);
-          }
-        });
+          chrome.storage.local.get([ videoId ], result => {
+            if(Object.keys(result).length === 0 && result.constructor === Object) {
+              chrome.storage.local.set({ [ videoId ]: 1 });
+              chrome.tabs.sendMessage(tab.id, { view: { id: videoId, count: 1 }});
+              console.log(`${time()} %c Views `, "color: white; background-color: #F56960", `${1}`);
+            } else {
+              let views = result[videoId] + 1;
+              chrome.storage.local.set({ [ videoId ]: views });
+              chrome.tabs.sendMessage(tab.id, { view: { id: videoId, count: views }});
+              console.log(`${time()} %c Views `, "color: white; background-color: #F56960", `${views}`);
+            }
+          });
+        }
       }
     }
   }
 });
+
+let time = () => {
+  let d = new Date();
+  let hours, meridiem;
+
+  if(d.getHours() <= 12) {
+    hours = d.getHours();
+    meridiem = 'AM';
+  } else {
+    hours = d.getHours() - 12;
+    meridiem = 'PM';
+  }
+
+  return `${hours}:${d.getMinutes()}:${d.getSeconds()} ${meridiem}`;
+}
